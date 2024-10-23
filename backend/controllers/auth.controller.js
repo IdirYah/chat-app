f9c23eeebc8344a4f303f48b7deff054a282f203
IdirYah <mi_yahiaoui@esi.dz>
2024-10-23 16:30:52 +0200

    auth with success

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,23 +1,71 @@
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+import generateTokenAndSetCookie from "../utils/generateToken.js"
+
 export const signup = async(req,res)=>{
     try {
-        
+        const {fullName,userName,password,confirmPassword,gender} = req.body
+        if(password !== confirmPassword){
+            return res.status(400).json({error:"Passwords don't match"})
+        }
+        const user = await User.findOne({userName})
+        if(user){
+            return res.status(400).json({error:"UserName already exists"})
+        }
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${userName}`;
+		const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${userName}`;
+        const newUser = new User({
+            fullName,
+            userName,
+            password:hashedPassword,
+            gender,
+            profilePic:gender === "male"?boyProfilePic:girlProfilePic,
+        })
+        generateTokenAndSetCookie(newUser._id,res)
+        await newUser.save()
+        res.status(201).json({
+            _id:newUser._id,
+            fullName:newUser.fullName,
+            userName:newUser.userName,
+            gender:newUser.gender,
+            profilePic:newUser.profilePic,
+        })
     } catch (error) {
-        
+        console.log("Error in signup controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
     }
 }
 
 export const login = async(req,res)=>{
     try {
-        
+        const {userName,password} = req.body
+        const user = await User.findOne({userName})
+        const isMatch = await bcrypt.compare(password,user?.password || "")
+        if(!isMatch || !user){
+            return res.status(400).json({error:"Invalid userName or password"})
+        }
+        generateTokenAndSetCookie(user._id, res);
+		res.status(200).json({
+			_id: user._id,
+			fullName: user.fullName,
+			username: user.userName,
+            gender:user.gender,
+			profilePic: user.profilePic,
+		});
     } catch (error) {
-        
+        console.log("Error in login controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
     }
 }
 
 export const logout = async(req,res)=>{
     try {
-        
+        res.cookie("jwt", "", { maxAge: 0 });
+		res.status(200).json({ message: "Logged out successfully" });
     } catch (error) {
-        
+        console.log("Error in logout controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
     }
 }
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "../backend/routes/auth.routes.js"
+import connectDB from "./db/connectDB.js"
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.use(express.json())
 app.use("/api/auth",authRoutes)
 
-app.listen(PORT,()=>console.log(`server running on port ${PORT}`))
+app.listen(PORT,()=>{
+    connectDB()
+    console.log(`server running on port ${PORT}`)
+})
