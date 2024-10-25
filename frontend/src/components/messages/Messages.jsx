245c6a061aee29a3b6c90bdc93bd3300762e1b0b
IdirYah <mi_yahiaoui@esi.dz>
2024-10-25 16:15:26 +0200

    UI design completed

---
+++ frontend/src/components/messages/Messages.jsx
@@ -0,0 +1,19 @@
+import Message from "./Message.jsx"
+
+const Messages = () => {
+  return (
+    <div className="px-4 overflow-auto flex-1">
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+    </div>
+  )
+}
+
+export default Messages
