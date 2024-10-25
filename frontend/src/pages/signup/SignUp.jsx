21e45253057644b223fb0eed406b917bd6d69bdd
IdirYah <mi_yahiaoui@esi.dz>
2024-10-25 19:56:57 +0200

    add signup fonctionnality

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,48 +1,64 @@
+import { Link } from "react-router-dom"
 import GenderCheckbox from "./GenderCheckbox"
+import { useState } from "react"
 
 const SignUp = () => {
+  const [inputs,setInputs] = useState({
+    fullName:"",
+    userName:"",
+    password:"",
+    confirmPassword:"",
+    gender:""
+  })
+  const handleCheckGender = (gender)=>{
+    setInputs({...inputs,gender:gender})
+  }
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    console.log(inputs);
+  }
   return (
-    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
-      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+    <div className="flex flex-col items-center justify-center mx-auto min-w-96">
+      <div className="w-full p-6 bg-gray-400 bg-opacity-0 rounded-lg shadow-md bg-clip-padding backdrop-filter backdrop-blur-lg">
         <h1 className="text-3xl font-semibold text-center text-gray-300">
           SignUp
           <span className="text-blue-500">ChatApp</span>
         </h1>
-        <from>
+        <form onSubmit={handleSubmit}>
           <div>
-            <label className="label p-2">
+            <label className="p-2 label">
               <span className="text-base label-text">FullName</span>
             </label>
-            <input type="text" placeholder="YAHIAOUI Idir" className="w-full input input-bordered h-10" />
+            <input type="text" placeholder="YAHIAOUI Idir" className="w-full h-10 input input-bordered" value={inputs.fullName} onChange={(e)=>setInputs({...inputs,fullName:e.target.value})} />
           </div>
           <div>
-            <label className="label p-2">
+            <label className="p-2 label">
               <span className="text-base label-text">UserName</span>
             </label>
-            <input type="text" placeholder="IdirYah" className="w-full input input-bordered h-10" />
+            <input type="text" placeholder="IdirYah" className="w-full h-10 input input-bordered" value={inputs.userName} onChange={(e)=>setInputs({...inputs,userName:e.target.value})} />
           </div>
           <div>
-            <label className="label p-2">
+            <label className="p-2 label">
               <span className="text-base label-text">Password</span>
             </label>
-            <input type="password" placeholder="Enter password" className="w-full input input-bordered h-10" />
+            <input type="password" placeholder="Enter password" className="w-full h-10 input input-bordered" value={inputs.password} onChange={(e)=>setInputs({...inputs,password:e.target.value})} />
           </div>
           <div>
-            <label className="label p-2">
+            <label className="p-2 label">
               <span className="text-base label-text">Confirm Password</span>
             </label>
-            <input type="password" placeholder="confirm password" className="w-full input input-bordered h-10" />
+            <input type="password" placeholder="confirm password" className="w-full h-10 input input-bordered" value={inputs.confirmPassword} onChange={(e)=>setInputs({...inputs,confirmPassword:e.target.value})} />
           </div>
-          <GenderCheckbox />
-          <a href="#" className="text-sm  hover:underline hover:text-blue-600 mt-2 inline-block">
+          <GenderCheckbox onCheckBoxChange={handleCheckGender} selectedGender={inputs.gender} />
+          <Link to="/login" className="inline-block mt-2 text-sm hover:underline hover:text-blue-600">
             Already have an account?
-          </a>
+          </Link>
           <div>
-            <button className="btn btn-block btn-sm mt-2">
+            <button className="mt-2 btn btn-block btn-sm">
               SignUp
             </button>
           </div>
-        </from>
+        </form>
       </div>
     </div>
   )
