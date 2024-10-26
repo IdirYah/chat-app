4d9f019e2d8993438704b7442dcf1040acdec959
IdirYah <mi_yahiaoui@esi.dz>
2024-10-26 19:18:45 +0200

    get conversations with success

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,15 +1,20 @@
 import Messages from "./Messages.jsx"
 import MessageInput from "./MessageInput.jsx"
 import { TiMessages } from "react-icons/ti"
+import useConversation from "../../zustand/useConversation.js"
+import { useEffect } from "react"
 
 const MessageContainer = () => {
-  const noChat = true
+  const {selectedConversation,setSelectedConversation} = useConversation()
+  useEffect(()=>{
+    return ()=>setSelectedConversation(null)
+  },[setSelectedConversation])
   return (
     <div className="md:min-w-[450px] flex flex-col">
-        {noChat ? <NoChatSelected /> : (
+        {!selectedConversation ? <NoChatSelected /> : (
             <>
-                <div className='bg-slate-500 px-4 py-2 mb-2'>
-                    <span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>IdirYah</span>
+                <div className='px-4 py-2 mb-2 bg-slate-500'>
+                    <span className='label-text'>To:</span> <span className='font-bold text-gray-900'>{selectedConversation.fullName}</span>
                 </div>
                 <Messages />
                 <MessageInput />
@@ -24,10 +29,10 @@ export default MessageContainer
 const NoChatSelected = () => {
 	return (
 		<div className="flex items-center justify-center w-full h-full">
-			<div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
+			<div className="flex flex-col items-center gap-2 px-4 font-semibold text-center text-gray-200 sm:text-lg md:text-xl">
 				<p>Welcome 👋 YAHIAOUI Idir ❄</p>
 				<p>Select a chat to start messaging</p>
-				<TiMessages className="text-3xl md:text-6xl text-center" />
+				<TiMessages className="text-3xl text-center md:text-6xl" />
 			</div>
 		</div>
 	);
--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,18 +1,21 @@
-import Conversation  from "./Conversation"
+import useGetConversations from "../../hooks/useGetConversations.js"
+import Conversation  from "./Conversation.jsx"
+import {getRandomEmoji} from "../../utils/emojis.js"
 
 const Conversations = () => {
+  const {loading,conversations} = useGetConversations()
+  console.log("Conversation: ",conversations)
   return (
-    <div className="py-2 flex flex-col overflow-auto">
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
+    <div className="flex flex-col py-2 overflow-auto">
+      {conversations.map((conversation,idx)=>(
+        <Conversation 
+        key={conversation._id}
+        conversation={conversation}
+        emoji={getRandomEmoji()}
+        lastIdx={idx === conversations.length-1}
+        />
+      ))}
+      {loading ? <span className='mx-auto loading loading-spinner'></span> : null}
     </div>
   )
 }
