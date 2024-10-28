a79d4ed8bb2a68872f1be18b9f718cc6867c8b7a
IdirYah <mi_yahiaoui@esi.dz>
2024-10-28 18:45:15 +0100

    deployment

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,11 @@ import messageRoutes from "../backend/routes/message.routes.js"
 import userRoutes from "../backend/routes/user.routes.js"
 import connectDB from "./db/connectDB.js"
 import { app,server } from "./socket/socket.js"
+import path from "path"
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
+const __dirname = path.resolve()
 
 app.use(express.json())
 app.use(cookieParser())
@@ -16,6 +18,11 @@ app.use(cookieParser())
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 server.listen(PORT,()=>{
     connectDB()
